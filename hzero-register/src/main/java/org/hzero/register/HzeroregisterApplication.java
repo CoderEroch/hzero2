325668476846be5eabf0b9d7dfb121997fb7c440
CoderEroch <36229410+CoderEroch@users.noreply.github.com>
2020-12-04 11:00:32 +0800

    Add files via upload

---
+++ hzero-register/src/main/java/org/hzero/register/HzeroregisterApplication.java
@@ -0,0 +1,17 @@
+package org.hzero.register;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
+@EnableChoerodonResourceServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HzeroregisterApplication {
+    
+        public static void main(String[] args) {
+            SpringApplication.run(HzeroregisterApplication.class, args);
+        }
+
+}
